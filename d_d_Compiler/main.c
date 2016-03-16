09ebef4b973a8742699121fc7db9ab5099cfc4fb
d_d <seancasey@sina.cn>
2016-03-16 16:30:56 +0800

    Initial Commit

---
+++ d_d_Compiler/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  d_d_Compiler
+//
+//  Created by d_d on 3/16/16.
+//  Copyright © 2016 ddapp. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
