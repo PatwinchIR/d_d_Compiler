dee2037b3aaef331e59ad907ea54d34cc4e24d0c
d_d <seancasey@sina.cn>
2016-05-25 15:22:46 +0800

    完成声明语句的翻译，包括嵌套过程，（多级符号表）。

--- d_d_Compiler/main.c
+++ d_d_Compiler/main.c
@@ -29,6 +29,7 @@
 #define FEOF 0
 #define ACCEPT 51
 #define TABLES 100
+#define DATATYPENUM 3
 
 #define TOBEDATATYPED 100
 
@@ -44,8 +45,16 @@ typedef struct idnode {
 struct ST{
     struct ST * previous;
     int depth;
+    int space_taken;
     Identifier * SymbolTableBody[BUCKETS];
 };
+
+typedef struct keywordnode{
+    char *name;
+    int type;
+}Keyword;
+
+Keyword *KeywordTable[BUCKETS];
 
 //Identifier *SymbolTableBody[BUCKETS];
 
@@ -73,10 +82,28 @@ int KeyWordCode[KEYWORDNUM] = {PROGRAM, PBEGIN, END, IF, THEN, ELSE,
     PROCEDURE, FUNCTION
 };
 
+char *TypeIdentifier[DATATYPENUM] = {
+    "Integer", "Real", "String"
+};
+
+int TypeWidth[DATATYPENUM] = {
+    INTWIDTH, REALWIDTH, STRWIDTH
+};
+
 // identifiers who's datatype field has not been filled.
 int tobeDatatyped[TOBEDATATYPED];
 int *tobe = tobeDatatyped;
 int *tobep = tobeDatatyped;
+
+//for string/int/real type variables;
+int currentDatatype = 0;
+
+//for subprocesses;
+int currentSubProc = -1;
+
+//for array type variables;
+int currentArray = 0;
+int currentArraySize = 0;
 
 //table pointer stack.
 struct ST *tblptrstk[TABLES];
@@ -96,24 +123,34 @@ float install_real(char *str);
 int install_oct(char *str);
 int install_hex(char *str);
 char* install_str(char *str);
-void initialiseSymbolTable();
+void initialiseKeywordTable();
 void cleantoken();
 int scan_token();
 int parse();
 
 struct ST* mktable(struct ST * previous);
-void enter(struct ST * table, char * name, int datatype, int offset);
+void enter(struct ST * table, int datatype);
+void addwidth(struct ST * table, int width);
+void enterproc(struct ST * table, int subproc, struct ST * subtable);
 
 int main(int argc, const char * argv[]) {
-    initialiseSymbolTable();
+    initialiseKeywordTable();
     pasfile = fopen("/Users/d_d/Desktop/CompilerPrinciple/hello.txt", "r");
     parse();
     return 0;
 }
 
 struct ST* mktable(struct ST * previous){
-    int i;
+    int i, pos, *temp;
     struct ST * newTable;
+    //To process newTable's name...
+    if (tobep != tobe) {
+        temp = tobe;
+        temp ++;
+        currentSubProc = *temp;
+        tobep = tobe;
+    }
+    
     newTable = (struct ST *)malloc(sizeof(struct ST));
     newTable->previous = previous;
     if (previous) {
@@ -121,29 +158,71 @@ struct ST* mktable(struct ST * previous){
     }else{
         newTable->depth = 0;
     }
+    newTable->space_taken = -1;
     for (i = 0; i < BUCKETS; i++) {
         newTable->SymbolTableBody[i] = (Identifier *)malloc(sizeof(Identifier));
         newTable->SymbolTableBody[i]->name = "";
         newTable->SymbolTableBody[i]->type = -1;
         newTable->SymbolTableBody[i]->datatype = -1;
+        newTable->SymbolTableBody[i]->offset = -1;
         newTable->SymbolTableBody[i]->next_ST = NULL;
         newTable->SymbolTableBody[i]->next_hash = NULL;
+    }
+    for (i = 0; i < DATATYPENUM; i++) {
+        pos = hashpjw(TypeIdentifier[i]);
+        newTable->SymbolTableBody[pos]->name = TypeIdentifier[i];
+        newTable->SymbolTableBody[pos]->type = TypeWidth[i];
     }
     return newTable;
 }
 
-void enter(struct ST * table, char * name, int datatype, int offset){
-    int pos = hashpjw(name);
-    table->SymbolTableBody[pos]->name = name;
-    table->SymbolTableBody[pos]->datatype = datatype;
-    
+void enter(struct ST * table, int datatype){
+    int pos;
+    Identifier * temp;
+    while (tobep != tobe) {
+        pos = *tobep;
+        temp = table->SymbolTableBody[pos];
+        while (temp) {
+            if (temp->datatype == -1) {
+                temp->datatype = datatype;
+                temp->offset = *offsetp;
+                if (currentArray) {
+                    printf("currentArray: %d\ncurrentArraySize: %d\n", currentArray, currentArraySize);
+                    *offsetp = *offsetp + datatype * currentArraySize;
+                    currentArray = 0;
+                    currentArraySize = 0;
+                }else{
+                    *offsetp += datatype;
+                }
+                tobep --;
+            }
+            temp = temp->next_hash;
+        }
+    }
 }
 
+void addwidth(struct ST * table, int width){
+    table->space_taken = width;
+}
+
+void enterproc(struct ST * table, int subproc, struct ST * subtable){
+    Identifier * temp;
+    temp = table->SymbolTableBody[subproc];
+    while (temp) {
+        if (temp->datatype == -1) {
+            table->SymbolTableBody[subproc]->next_ST = subtable;
+        }
+        temp = temp->next_hash;
+    }
+}
 
 int parse() {
     int state; /* current state */
     int n;
     int i;
+    
+    //for print debug;
+    int k;
     
     /* State Stack*/
     int ssa[INITDEPTH];
@@ -190,6 +269,25 @@ int parse() {
             printf("\nEntering State %d\nNext Token: ", *ssp);
             
             look_ahead = scan_token();
+            
+            //for dbg; declaration translation.
+//            temp = *tblptrp;
+//            if (temp) {
+//                for (k = 0; k < BUCKETS; k++) {
+//                    if (temp->SymbolTableBody[k]->datatype != -1) {
+//                        printf("name: %-5s ", temp->SymbolTableBody[k]->name);
+//                        printf("type: %d ", temp->SymbolTableBody[k]->type);
+//                        printf("datatype: %d ", temp->SymbolTableBody[k]->datatype);
+//                        printf("offset: %d\n", temp->SymbolTableBody[k]->offset);
+//                    }
+//                }
+//            }
+            
+            if (look_ahead == 0) {
+                currentArray = 1;
+            }
+            
+            
         }else if (n < 0) {
             POPSTACK(ProductionRightLENGTH[-n-1]);
             state = *ssp;
@@ -203,12 +301,19 @@ int parse() {
             printf("Reduce using %d: %s\n",-n-1, ProductionTable[-n-1]);
             
             switch (-n-1) {
+                case 5: //program -> program_heading semicolon block DOT
+                    addwidth(*tblptrp, *offsetp);
+                    tblptrp --;
+                    offsetp --;
+                    break;
+                    
                 case 14: //m1 -> EPSILON
                     tblptrp ++;
                     *tblptrp = mktable(NULL);
                     offsetp ++;
                     *offsetp = 0;
                     break;
+                    
                 case 48: //n1 -> EPSILON
                     temp = mktable(*tblptrp);
                     tblptrp ++;
@@ -217,13 +322,29 @@ int parse() {
                     offsetp ++;
                     *offsetp = 0;
                     break;
+                    
                 case 115: //variable_declaration -> identifier_list COLON type_denoter
+                    enter(*tblptrp, currentDatatype);
+                    break;
+                    
+                case 138: //procedure_declaration -> procedure_heading semicolon n1 procedure_block
+                    temp = *tblptrp;
+                    addwidth(temp, *offsetp);
+                    tblptrp --;
+                    offsetp --;
+                    enterproc(*tblptrp, currentSubProc, temp);
+                    break;
+                    
+                case 149:
+                    
+                    break;
                     
                 default:
                     break;
             }
             
         }else if (n == 1353) {
+            
             printf("\nCongrats! ACCEPT !\n\n");
             return 1;
         }else {
@@ -499,23 +620,58 @@ int hashpjw(char *str) {
 
 int gettoken(char *str) {
     int pos = hashpjw(str);
-    if (SymbolTableBody[pos]->type != IDENTIFIER && SymbolTableBody[pos]->type != -1) {
-        return SymbolTableBody[pos]->type;
+    if (KeywordTable[pos]->type != -1) {
+        return KeywordTable[pos]->type;
     }
     return IDENTIFIER;
 }
 
+int isDatatype(char *str){
+    int i;
+    for (i = 0; i < DATATYPENUM; i++) {
+        if (strcmp(str, TypeIdentifier[i]) == 0) {
+            return 1;
+        }
+    }
+    return 0;
+}
+
 int install_id(char *str) {
     struct ST * sttemp;
-    int pos = hashpjw(str);
+    Identifier *idnodetemp;
+    int pos, typetemp;
+    char *strtemp;
+    strtemp = (char *)malloc(100 * sizeof(char));
+    strcpy(strtemp, str);
+    if (gettoken(str) != IDENTIFIER) {
+        return 0;
+    }
     if (*tblptrp != NULL) {
+        pos = hashpjw(str);
         sttemp = *tblptrp;
-        if (sttemp->SymbolTableBody[pos]->type != IDENTIFIER && sttemp->SymbolTableBody[pos]->type != -1) {
-            return 0;
-        }else{
-            sttemp->SymbolTableBody[pos]->name = str;
+        typetemp = sttemp->SymbolTableBody[pos]->type;
+        if (typetemp == -1) {
+            sttemp->SymbolTableBody[pos]->name = strtemp;
             sttemp->SymbolTableBody[pos]->type = IDENTIFIER;
             sttemp->SymbolTableBody[pos]->next_hash = NULL;
+            tobep ++;
+            *tobep = pos;
+        }else if(isDatatype(str)){
+            currentDatatype = typetemp;
+        }else if(!isDatatype(str)){
+            idnodetemp = sttemp->SymbolTableBody[pos];
+            while (idnodetemp->next_hash) {
+                idnodetemp = idnodetemp->next_hash;
+            }
+            idnodetemp->next_hash = (Identifier *)malloc(sizeof(Identifier));
+            idnodetemp->next_hash->name = str;
+            idnodetemp->next_hash->type = IDENTIFIER;
+            idnodetemp->next_hash->datatype = -1;
+            idnodetemp->next_hash->offset = -1;
+            idnodetemp->next_hash->next_hash = NULL;
+            idnodetemp->next_hash->next_ST = NULL;
+            tobep ++;
+            *tobep = pos;
         }
     }
 //            if (SymbolTableBody[pos]->type != IDENTIFIER && SymbolTableBody[pos]->type != -1) {
@@ -526,13 +682,13 @@ int install_id(char *str) {
 //                SymbolTableBody[pos]->next_hash = NULL;
 //            }
     //To store those identifiers with the same datatype temperarily.
-    tobep ++;
-    *tobep = pos;
-    
     return pos;
 }
 
 int install_int(char *str) {
+    if (currentArray) {
+        currentArraySize = atoi(str);
+    }
     return atoi(str);
 }
 
@@ -552,9 +708,18 @@ char* install_str(char *str) {
     return str;
 }
 
-void initialiseSymbolTable() {
+void initialiseKeywordTable() {
     int i, pos;
-    
+    for (i = 0; i < BUCKETS; i++) {
+        KeywordTable[i] = (Keyword *)malloc(sizeof(Keyword));
+        KeywordTable[i]->name = "";
+        KeywordTable[i]->type = -1;
+    }
+    for (i = 0; i < KEYWORDNUM; i++) {
+        pos = hashpjw(KeyWord[i]);
+        KeywordTable[pos]->name = KeyWord[i];
+        KeywordTable[pos]->type = KeyWordCode[i];
+    }
 //    for (i = 0; i < BUCKETS; i++) {
 //        SymbolTableBody[i] = (Identifier *)malloc(sizeof(Identifier));
 //        SymbolTableBody[i]->name = "";
